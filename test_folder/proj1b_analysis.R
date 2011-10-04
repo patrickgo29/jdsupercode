e350b86df12aebf9244c24750f67e0b082689bc8
jack.hs.chua <jack.hs.chua@gmail.com>
2011-10-04 18:00:20 +0000

    Mostly completed plotting script in R, plus tar file with all the plots

--- test_folder/proj1b_analysis.R
+++ test_folder/proj1b_analysis.R
@@ -4,38 +4,134 @@ library('gridExtra')
 library('ggplot2')
 library('reshape2')
 
-system("cd /home/jack/Dropbox/source/cse6230/proj1");
+setwd("/home/jack/Dropbox/source/cse6230/project1");
+results_folder="Results2/";
 
 ###### DO DATA STUFF #############################
-summa_data = read.table("Results/mm_singlenode.csv",sep=",");
-localmm_data = read.table("Results/summa_singlenode.csv",sep=",");
+#
+# TIME_MM: mm, nodes, ppn, tpp, imp, m, n, k, tet, trials, tpt
+# SUMMA: summa, nodes, ppn, tpp, imp, m, n, k, x, y, b, tet, trials, tpt
 
-colnames(summa_data) <- c("nodes","ppn","m","n","k","px","py","panel_size","time","trials","timepertrial","type","threads");
-colnames(localmm_data) <- c("nodes","ppn","m","n","k","time","trials","timepertrial","threads");
+ssn_data = read.table(paste(results_folder,"summa_single_node.csv",sep=""),sep=",");
+mm_sn_data = read.table(paste(results_folder,"mm_single_node.csv",sep=""),sep=",");
+mn_data = read.table(paste(results_folder,"multiple_nodes.csv",sep=""),sep=",");
 
-summa_castdata = ddply(summa_data, c("nodes","procs","m","n","k","px","py","panel_size","threads","type"), function(df) data.frame(meantime=mean(df$timepertrial)));
-local_mmdata = ddply(localmm_data, c("m","n","k","time","trials","timepertrial"), function(df) data.frame(meantime=mean(df$timepertrial)));
+colnames(ssn_data) <- c("alg","nodes","ppn","tpp","imp","m","n","k","x","y","b","tet","trials","tpt");
+colnames(mm_sn_data) <- c("alg","nodes","ppn","tpp","imp","m","n","k","tet","trials","tpt");
+colnames(mn_data) <- c("alg","nodes","ppn","tpp","imp","m","n","k","x","y","b","tet","trials","tpt");
 
-m_s=summa_castdata$m; m_l = localmm_castdata$m;
-n_s=summa_castdata$n; n_l = localmm_castdata$n;
-k_s=summa_castdata$k; k_l = localmm_castdata$k;
-px_s=summa_castdata$px; px_l = localmm_castdata$px;
-py_s=summa_castdata$py; py_l = localmm_castdata$py;
-pb_s=summa_castdata$panel_size; pb_l = localmm_castdata$panel_size;
-threads_s=summa_castdata$threads; threads_l = localmm_castdata$threads;
-type_s=summa_castdata$type; type_l = localmm_castdata$type;
-tpt_s=summa_castdata$timepertrial; tpt_l = localmm_castdata$timepertrial;
+# Refactor data
+ssn_data <- transform(ssn_data, nodes=as.numeric(nodes), ppn=as.numeric(ppn),
+                      tpp=as.numeric(tpp),m=as.numeric(m),n=as.numeric(n),
+                      k=as.numeric(k),x=as.numeric(x),y=as.numeric(y),
+                      b=as.numeric(b),tet=as.numeric(tet),trials=as.numeric(trials),
+                      tpt=as.numeric(tpt), alg=as.character(alg), 
+                      imp=as.character(imp));
+mm_sn_data <- transform(mm_sn_data, nodes=as.numeric(nodes), ppn=as.numeric(ppn),
+                      tpp=as.numeric(tpp),m=as.numeric(m),n=as.numeric(n),
+                      k=as.numeric(k),tet=as.numeric(tet),trials=as.numeric(trials),
+                      tpt=as.numeric(tpt), alg=as.character(alg), 
+                      imp=as.character(imp));
+mn_data <- transform(mn_data, nodes=as.numeric(nodes), ppn=as.numeric(ppn),
+                      tpp=as.numeric(tpp),m=as.numeric(m),n=as.numeric(n),
+                      k=as.numeric(k),x=as.numeric(x),y=as.numeric(y),
+                      b=as.numeric(b),tet=as.numeric(tet),trials=as.numeric(trials),
+                      tpt=as.numeric(tpt), alg=as.character(alg), 
+                      imp=as.character(imp));
+
+# Transform time to milliseconds
+ssn_data$tpt_mil = 1000*ssn_data$tpt;
+mm_sn_data$tpt_mil = 1000*mm_sn_data$tpt;
+mn_data$tpt_mil = 1000*mn_data$tpt;
+
+# Transform time to log scale
+ssn_data$logtpt = log2(ssn_data$tpt_mil);
+mm_sn_data$logtpt = log2(mm_sn_data$tpt_mil);
+mn_data$logtpt = log2(mn_data$tpt_mil);
 
 ###### PLOTS #######################################################
-# 1. Compare local_mm implementations on single node and multiple nodes
-#     - For single node, vary different sizes over different threads for 
-#       OpenMP and MKL. Compare this with Naive implementation over processes
-#       instead of threads.
-#     - For multi node, 
-# 2. Compare summa on single node and multiple nodes
-#     - For single node, vary different sizes, different panels, and 
-#       different threads 
-#     - For multi node, vary different nodes, sizes, panels
+# 1. Plots for single node matrix multiplication. 
+# 2. Plots for single node SUMMA multiplication.
+# 3. Plots for multi node SUMMA multiplication.
 #
-#
+# Note: by default, ggplot always clears the screen and uses the entire device
+# 
+# Note: names specify implementation, number of nodes, and panel block size
 
+# 1. plots for single node matrix multiply (MM_SN)
+for (i in unique(mm_sn_data$imp)) {          # for each unique implementation,
+    for (j in unique(mm_sn_data$nodes)) {    # and node count
+         # get the subset of data we want
+         tempdata = mm_sn_data[mm_sn_data$imp==i & mm_sn_data$nodes==j,];
+
+         # naming
+         tempname = paste("mm_sn-",as.character(i),"-",as.character(j),
+                             "nodes",sep="");
+            
+         # plot and save to the specified savefolder and filename
+         p <- ggplot(tempdata, aes(x=tpp, y=logtpt, group=m));
+         p + geom_line(aes(colour=m)) + opts(title=tempname);
+         ggsave(paste(results_folder,tempname,".pdf",sep=""),scale=1);
+    }
+}
+
+# 2. plot single nodes for 'naive' case. we're doing this because
+# we are using threads instead of processes. only iterate over panel block sizes
+for (k in unique(ssn_data$b)) {
+    tempdata = ssn_data[ssn_data$imp=='naive' & ssn_data$b==k,];
+    tempname = paste("ssn-naive-",as.character(k),"panel",sep="");
+    p <- ggplot(tempdata, aes(x=ppn, y=logtpt, group=m));
+    p + geom_line(aes(colour=m)) + opts(title=tempname);
+    ggsave(paste(results_folder,tempname,".pdf",sep=""),scale=1);
+}
+
+# 3. plots for multiple nodes (MN)
+# max 8 threads per node
+# only openMP/MKL uses threads
+for (i in unique(mn_data$imp)[2:3]) {         # for each non-naive implementation  
+    for (j in unique(mn_data$nodes)) {        # node count,
+        for (k in unique(mn_data$b)) {        # panel size,
+            tempdata = mn_data[mn_data$imp==i &
+                               mn_data$nodes==j &
+                               mn_data$b==k,];
+            tempname = paste("mn-",as.character(i),"-",as.character(j),"nodes",
+                             "-",as.character(k),"panel",sep="");
+
+            p <- ggplot(tempdata, aes(x=ppn, y=logtpt, group=m));
+            p + geom_line(aes(colour=m)) + opts(title=tempname);
+            ggsave(paste(results_folder,tempname,".pdf",sep=""),scale=1);
+        }
+    }
+}
+
+# 3(cont). plot multiple nodes in 'naive' case. doing this because 
+# naive case does not vary in threads like the others do. iterate over
+# panel block size and nodes
+for (k in unique(mn_data$b)) {
+        tempdata = mn_data[mn_data$imp=='naive' & mn_data$b==k,];
+        tempname = paste("mn-naive-",as.character(k),"panel",sep="");
+        p <- ggplot(tempdata, aes(x=nodes,y=logtpt,group=m));
+        p + geom_line(aes(colour=m)) + opts(title=tempname);
+        ggsave(paste(results_folder,tempname,".pdf",sep=""),scale=1);
+}
+
+# clean up
+rm(results_folder,tempdata,tempname);
+
+###### ANALYSIS ######################################
+# 1. Print configuration with lowest tpt for each size matrix
+
+for (i in unique(ssn_data$m)) {
+    tempdata = ssn_data[ssn_data$m==i,];
+    print(tempdata[tempdata$tpt==min(tempdata$tpt),])
+}
+for (i in unique(mm_sn_data$m)) {
+        tempdata = mm_sn_data[mm_sn_data$m==i,];
+        print(tempdata[tempdata$tpt==min(tempdata$tpt),])
+}  
+for (i in unique(mn_data$m)) {
+        tempdata = mn_data[mn_data$m==i,];
+        print(tempdata[tempdata$tpt==min(tempdata$tpt),])
+}  
+
+# 2. 
