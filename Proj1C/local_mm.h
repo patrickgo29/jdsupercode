fe409c90e36a9b8e4994d09d16ac69e4e5df21a0
david.noble.jr <david.noble.jr@092daf8e-cf9d-87bc-2db1-fad5daf9638a>
2011-10-16 19:49:45 +0000

    intermittent level

--- Proj1C/local_mm.h
+++ Proj1C/local_mm.h
@@ -11,39 +11,64 @@
 #define MKL 2
 
 void local_mm(const int m, const int n, const int k,
-              const double alpha,
-              const double *A, const int lda, 
-              const double *B, const int ldb,
-              const double beta, 
-              double *C, const int ldc);
+	const double alpha,
+	const double *A, const int lda, 
+	const double *B, const int ldb,
+	const double beta, 
+	double *C, const int ldc);
 
 void local_mm_openmp(const int m, const int n, const int k,
-              const double alpha,
-              const double *A, const int lda, 
-              const double *B, const int ldb,
-              const double beta, 
-              double *C, const int ldc);
+	const double alpha,
+	const double *A, const int lda, 
+	const double *B, const int ldb,
+	const double beta, 
+	double *C, const int ldc);
+
+void local_mm_openmp_cbl_inner(const int m, const int n, const int k,
+	const double alpha,
+	const double *A, const int lda,
+	const double *B, const int ldb,
+	const double beta,
+	double *C, const int ldc,
+	int l1, int l2, int l3);
+
+void local_mm_openmp_cbl_outer(const int m, const int n, const int k,
+	const double alpha,
+	const double *A, const int lda,
+	const double *B, const int ldb,
+	const double beta,
+	double *C, const int ldc,
+	int l1, int l2, int l3);
+
+void local_mm_openmp_cbl_cop_inner(const int m, const int n, const int k,
+	const double alpha,
+	const double *A, const int lda,
+	const double *B, const int ldb,
+	const double beta,
+	double *C, const int ldc,
+	int l1, int l2, int l3);
+
+void local_mm_openmp_cbl_cop_outer(const int m, const int n, const int k,
+	const double alpha,
+	const double *A, const int lda,
+	const double *B, const int ldb,
+	const double beta,
+	double *C, const int ldc,
+	int l1, int l2, int l3);
 
 void local_mm_mkl(const int m, const int n, const int k,
-              const double alpha,
-              const double *A, const int lda, 
-              const double *B, const int ldb,
-              const double beta, 
-              double *C, const int ldc);
+	const double alpha,
+	const double *A, const int lda, 
+	const double *B, const int ldb,
+	const double beta, 
+	double *C, const int ldc);
 
-void local_mms(mat_mul_specs * mms,
-                const double alpha,
-                const double *A, const int lda,
-                const double *B, const int ldb,
-                const double beta,
-                double *C, const int ldc);
-
-void local_mm_typed(const int m, const int n, const int k,
-                    const double alpha,
-                    const double *A, const int lda,
-                    const double *B, const int ldb,
-                    const double beta,
-                    double *C, const int ldc,
-                    int type);
+void local_mm_mms(const int m, const int n, const int k,
+	const double alpha,
+	const double *A, const int lda,
+	const double *B, const int ldb,
+	const double beta,
+	double *C, const int ldc,
+	mat_mul_specs * mms);
 
 #endif
